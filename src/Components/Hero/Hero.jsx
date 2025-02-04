61ea3885d1391aade1aee7bdb3f1f92502e13432
dennisjacob03 <dennispj03@gmail.com>
2025-02-04 21:20:17 +0530

    third commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,14 +3,57 @@ import './Hero.css'
 
 const Hero = () => {
 	return (
-		<div className='hero container'>
-			<div className="hero-text">
-				<h1>BOOK YOUR</h1><br />
-				<h1 className='text-slide'>TICKETS FOR</h1>
-				<h1 className='text-fade'>SPORT</h1>
-			</div>
-		</div>
-	)
+    <div className="hero container">
+      <div className="hero-combined">
+        <div className="hero-text">
+          <h1>BOOK YOUR</h1>
+          <h1 className="text-slide">
+            TICKETS FOR <span className="text-fade">SPORT</span>
+          </h1>
+        </div>
+        <div className="filter">
+          <div className="filter-form">
+            <form className="form-items">
+              <div className="search">
+                <input type="text" placeholder="Search for Sports" />
+                <button type="submit">
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+              </div>
+              <div className="vl"></div>
+              <div className="other-filter">
+                <div className="img">
+                  <i className="fa-solid fa-city"></i>
+                </div>
+                <span className="type">City</span>
+                <select className="select-bar">
+                  <option value="all">All</option>
+                  <option value="ahmedabad">Ahmedabad</option>
+                  <option value="bangalore">Bangalore</option>
+                  <option value="chennai">Chennai</option>
+                  <option value="guwahati">Guwahati</option>
+                  <option value="hyderabad">Hyderabad</option>
+                  <option value="kochi">Kochi</option>
+                  <option value="kolkata">Kolkata</option>
+                  <option value="mumbai">Mumbai</option>
+                  <option value="raipur">Raipur</option>
+                  <option value="thiruvananthapuram">Thiruvananthapuram</option>
+                </select>
+              </div>
+              <div className="vl"></div>
+              <div className="other-filter">
+                <div className="img">
+                  <i className="fa-solid fa-calendar-days"></i>
+                </div>
+                <span className="type">Date</span>
+                <input type="date" className='date'/>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logowhite.png'
+import React, { useEffect } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logowhite.png";
 
 const Navbar = () => {
-	return (
-		<nav className='container'>
-			<img src={logo} alt="SpotOn" className='logo'/>
-			<ul>
-				<li>Explore</li>
-				<li>Favourites</li>
-				<li>My Tickets</li>
-				<li>Contact</li>
-				<li><button className='btn'>SIGN IN</button></li>
-			</ul>
-		</nav>
-	)
-}
 
-export default Navbar
+	const [sticky, setSticky] = React.useState(false);
+	React.useEffect(() => {
+		window.addEventListener("scroll", () => {
+			window.scrollY > 50 ? setSticky(true) : setSticky(false);
+		});
+	},[]);
+
+
+  return (
+    <nav className={`container ${sticky ? "sticky" : ""}`}>
+      <img src={logo} alt="SpotOn" className="logo" />
+      <ul>
+        <li>Explore</li>
+        <li>My Tickets</li>
+        <li>Contact</li>
+        <li>
+          <button className="btn">SIGN IN</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
